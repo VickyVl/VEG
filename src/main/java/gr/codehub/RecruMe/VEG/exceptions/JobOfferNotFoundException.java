82a2762e924f8bb39c4cd9eee94e67774ef52e52
Georgia <lgeorgia95@gmail.com>
2020-02-08 14:35:51 +0200

    JOBOFFER CONTROLLER AND SERVICE vol2

---
+++ src/main/java/gr/codehub/RecruMe/VEG/exceptions/JobOfferNotFoundException.java
@@ -0,0 +1,7 @@
+package gr.codehub.RecruMe.VEG.exceptions;
+
+public class JobOfferNotFoundException  extends Exception {
+    public JobOfferNotFoundException(String description) {
+        super(description);
+    }
+}
---
+++ src/main/java/gr/codehub/RecruMe/VEG/models/ApplicantSkill.java
@@ -0,0 +1,22 @@
+/*
+package gr.codehub.RecruMe.VEG.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class ApplicantSkill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JsonIgnore
+    private Skill skill;
+}
+*/
