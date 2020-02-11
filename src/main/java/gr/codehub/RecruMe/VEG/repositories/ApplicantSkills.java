b33cde65ec09af3c90077a31c2c4c98a4eaf71be
teo <teo@teo.gr>
2020-02-11 13:22:46 +0200

    SEARCH BY SKILL JOBOFFER, APPLICANT

--- src/main/java/gr/codehub/RecruMe/VEG/repositories/ApplicantSkills.java
+++ src/main/java/gr/codehub/RecruMe/VEG/repositories/ApplicantSkills.java
@@ -2,9 +2,13 @@ package gr.codehub.RecruMe.VEG.repositories;
 
 import gr.codehub.RecruMe.VEG.models.Applicant;
 import gr.codehub.RecruMe.VEG.models.ApplicantSkill;
+import gr.codehub.RecruMe.VEG.models.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ApplicantSkills extends JpaRepository<ApplicantSkill, Integer> {
+    List<ApplicantSkill> findBySkillId(int id);
 }
